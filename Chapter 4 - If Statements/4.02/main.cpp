e0e95b6f22c3c8ebb6eaab003b1dc03b052aac87
Sh1ng0 <55451329+Sh1ng0@users.noreply.github.com>
2019-09-17 17:11:34 +0200

    Update main.cpp

--- Chapter 4 - If Statements/4.02/main.cpp
+++ Chapter 4 - If Statements/4.02/main.cpp
@@ -16,13 +16,41 @@ so that either of two numbers are valid, but use only one if statement to do the
 using namespace std;
 
 int main() {
-  int inputedPassword;
-  int password = 500;
+  int inputedPassword; 
+  int password = 500; //Practice wants you to accept two numbers for a password, this integer is declared as 500 which is oly one number.
   cout << "Please enter your password. (Must be in the form of a number)" << endl;
   cin >> inputedPassword;
-  if (inputedPassword == password) {
+  if (inputedPassword == password) { //In order for the password to acept two different passwords we can instead the boolean opeartor OR
     cout << "Access Grained" << endl;
   } else {
     cout << "Access Denied" << endl;
   }
 }
+
+Correct answer:
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+
+
+    cout << "Please enter your password: " << endl;
+    cout << "Password: ";
+    cin >> a;
+
+    if ( a == 1500 || a == 1234)
+    {
+        cout << "Acces granted" << endl;
+    }
+    else
+    {
+        cout << "Acces denied" << endl;
+    }
+    return 0;
+}
+
+
